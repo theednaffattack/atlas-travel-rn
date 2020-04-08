34bb0999f1e8670d7959c7a43d33727d25d2f2c6
eddie naff <eddienaff@eddienaff.com>
2020-04-08 10:47:56 -0700

    Add auth provider to simplify code organization

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,3 +1,18 @@
+import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 import { client } from "./apollo";
+import { AuthProvider } from "./auth-provider";
+import { Routes } from "./routes";
+
+interface ProvidersProps {}
+
+export const Providers: React.FC<ProvidersProps> = ({}) => {
+  return (
+    <AuthProvider>
       <ApolloProvider client={client}>
+        <Routes />
       </ApolloProvider>
+    </AuthProvider>
+  );
+};
