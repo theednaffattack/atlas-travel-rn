bde7abd0898181485fe65c35804ca8e7200ebc62
eddie naff <eddienaff@eddienaff.com>
2020-04-08 16:41:12 -0700

    Replace home and search with nav Stacks

--- src/app-tabs.tsx
+++ src/app-tabs.tsx
@@ -1,37 +1,43 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from "@expo/vector-icons";
+
 import { AppParamList } from "./app-param-list";
-import { Center } from "./center";
-import { Button, Text } from "react-native";
-import { AuthContext } from "./auth-provider";
+import { HomeStack } from "./home-stack";
+import { SearchStack } from "./search-stack";
 
 interface AppTabsProps {}
 
 const Tabs = createBottomTabNavigator<AppParamList>();
 
-function Home() {
-  const { logout } = React.useContext(AuthContext);
-  return (
-    <Center>
-      <Text>home</Text>
-      <Button title="logout" onPress={() => logout()} />
-    </Center>
-  );
-}
-
-function Search() {
-  return (
-    <Center>
-      <Text>search</Text>
-    </Center>
-  );
-}
-
 export const AppTabs: React.FC<AppTabsProps> = ({}) => {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Home" component={Home} />
-      <Tabs.Screen name="Search" component={Search} />
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home"; // focused
+            // ? "ios-information-circle"
+            // : "ios-information-circle-outline";
+            return <AntDesign name={iconName} size={size} color={color} />;
+          } else if (route.name === "Search") {
+            // iconName = focused ? "ios-list-box" : "ios-list";
+            iconName = "search1";
+
+            // You can return any component that you like here!
+            return <AntDesign name={iconName} size={size} color={color} />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tabs.Screen name="Home" component={HomeStack} />
+      <Tabs.Screen name="Search" component={SearchStack} />
     </Tabs.Navigator>
   );
 };
