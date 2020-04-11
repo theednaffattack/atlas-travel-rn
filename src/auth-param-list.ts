db49d585dc77a6cea3e11ea6e3dae667badebb02
eddie naff <eddienaff@eddienaff.com>
2020-04-11 13:14:33 -0700

    Add home screen to auth params

--- src/auth-param-list.ts
+++ src/auth-param-list.ts
@@ -2,6 +2,7 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { RouteProp } from "@react-navigation/native";
 
 export type AuthParamList = {
+  Home: undefined;
   Login: undefined;
   Register: undefined;
 };
