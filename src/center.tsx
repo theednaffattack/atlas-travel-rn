f1bc29e32019564286ed02ea655807b7bf25c2d0
eddie naff <eddienaff@eddienaff.com>
2020-04-11 13:15:18 -0700

    Add new custom view components

--- src/center.tsx
+++ src/center.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { View } from "react-native";
+import styled from "styled-components/native";
+import { color, ColorProps } from "styled-system";
 
 interface CenterProps {}
+
+export const MyView = styled.View<ColorProps>`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  /* ${color} */
+`;
 
 export const Center: React.FC<CenterProps> = ({ children }) => {
   return (
@@ -15,4 +24,11 @@ export const Center: React.FC<CenterProps> = ({ children }) => {
       {children}
     </View>
   );
+};
+
+export const HomeCenter: React.FC<CenterProps & ColorProps> = ({
+  bg,
+  children,
+}) => {
+  return <MyView bg={bg}>{children}</MyView>;
 };
